e1cbe98f727652e9f8787a2a5558c424fafa1954
Scott Bromander <scottbromander@gmail.com>
2021-03-12 20:11:23 -0600

    adds in User entries

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # in production, that would be the url, here this is local. Echo adds info
-engine = create_engine('sqlite:///users.db', echo=True)
+engine = create_engine('sqlite:///users.db', echo=False)
+# Binds our sessions to the database
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 # model is the table and columns
@@ -22,3 +26,33 @@ class User(Base):
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+
+    # Just created. Not added to the database yet
+    # scott_user = User(name='Scott', fullname='Scott Bromander', nickname='Bromander')
+    # print(scott_user.name)
+    # print(scott_user.id)
+    # # Stages, but not committed to the database yet
+    # session.add(scott_user)
+    # # Adds our user to the database
+    # session.commit()
+    # print(scott_user.id)
+
+    # Adding multiple entries at once, using add_all
+    # session.add_all([
+    #     User(name='Jackson', fullname='Jackson Bromander', nickname='Jack'),
+    #     User(name='Rachael', fullname='Rachael Bromander', nickname='Rach'),
+    #     User(name='Scott', fullname='Scott Bromander', nickname='Bromander')
+    # ])
+
+    # Here we are breaking it across multiple users
+    new_users = [
+        User(name='Jackson', fullname='Jackson Bromander', nickname='Jack'),
+        User(name='Rachael', fullname='Rachael Bromander', nickname='Rach'),
+        User(name='Scott', fullname='Scott Bromander', nickname='Bromander')
+    ]
+
+    session.add_all(new_users)
+    session.commit()
+
+    for user in new_users:
+        print(user.fullname, user.id)
--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@
 - `source ./env/bin/activate` - activates the venv
 - `pip freeze > requirements.txt` - create requirements for the project, including sqlalchemy
 - `models.py` - Typical pattern to create DB model in models.py
+
+### Checking SQLLite
+- `sqlite3 users.db`
+- `.tables`
+- `SELECT * FROM users;`
